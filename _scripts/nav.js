db887634222f3e741c9600ad87ec2289e3ab9093
Lindsey Maddox <lindseyemaddox@Neeleys-MacBook-Pro.local>
2018-02-26 00:11:02 -0600

    work through slides

--- _scripts/nav.js
+++ _scripts/nav.js
@@ -1,7 +1,7 @@
 (function($) {
 
   $(document).ready(function(){
-    $('.toggle-nav').on('click', function() {
+    $('.droppers').on('click', function() {
       toggleNavigation($(this), $('.nav-pane'));
       changeLetters($(this));
     });
